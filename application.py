823c8115321b15233053b966abe20dd3ff3896f7
cdiazufano <clanufano@gmail.com>
2020-07-05 07:01:57 +0200

    Seek realizado falta logout

--- application.py
+++ application.py
@@ -23,11 +23,12 @@ def index():
             user = db.execute("SELECT name, password FROM users WHERE  password= :inpass",
                     {"inpass": usrpass}).fetchone()
             if user is None:
-                error='El Usuario no esta registrado'
+                error='The user is not registered'
                 return render_template('error.html', message=error) 
             else:
-                msg='el usuario esta en la BBDD.'
-                return render_template('exito.html', message=msg)            
+                msg='the user exist in the DataBase.'
+                # add user=user
+                return render_template('seek.html')            
         if button_click == 'Register':
             user = db.execute("SELECT name, password FROM users WHERE  password= :inpass",
                     {"inpass": usrpass}).fetchone()
@@ -35,11 +36,37 @@ def index():
                 db.execute("INSERT INTO users (name, password) VALUES (:name, :password)",
                 {"name": usrname, "password": usrpass})
                 db.commit()
-                msg='se ha registrado correctamente.'
+                msg='It has been successfully registered.'
                 return render_template('exito.html', message=msg)
             else:
-                msg='el usuario esta en la BBDD.'
+                msg='the user exist in the DataBase.'
                 return render_template('exito.html', message=msg)  
         return render_template('index.html') 
-    return render_template('index.html')    
+    return render_template('index.html') 
+
+@app.route('/seek', methods=['GET', 'POST'])
+def seek():
+    button_click= request.values.get('logout')
+    if button_click == 'logout':
+            user=None
+            return render_template('index.html', user=user) 
+    seek = request.values.get("seek")
+    if seek == "":
+        msg= 'You must enter a search model.'
+        return render_template('error.html', message=msg)
+    else: 
+        seek='%'
+        seek += request.values.get("seek")
+        seek+='%'
+        lisbn =  db.execute("SELECT * from goodbooks WHERE  isbn like :seek",
+                    {"seek": seek}).fetchall()
+        ltitle =  db.execute("SELECT * from goodbooks WHERE  title like :seek",
+                    {"seek": seek}).fetchall()
+        lauthor = db.execute("SELECT * from goodbooks WHERE  author like :seek",
+                    {"seek": seek}).fetchall() 
+        if (len(lisbn) < 1 and len(ltitle) < 1 and len(lauthor) < 1 ):
+            msg='There is no result for that search'
+            return render_template('seek.html', message=msg) 
+        else:                      
+            return render_template('seek.html', lisbn=lisbn, ltitle=ltitle, lauthor=lauthor)        
 app.run()
