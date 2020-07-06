bec05d55e19335631615fdc4431049d39fb84d05
cdiazufano <clanufano@gmail.com>
2020-07-06 12:59:28 +0200

    Falta añadir comentarios  BD en /book y Json salida info

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
 import os
 import requests
-#import json
 
 
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_session import Session
 
 app = Flask(__name__)
+app.secret_key = '11887010'
 
 app.run
 
@@ -23,7 +25,7 @@ def index():
         usrpass =request.values.get("password")
         button_click= request.values.get('sub_button')
         if button_click == 'Login':
-            user = db.execute("SELECT name, password FROM users WHERE  password= :inpass",
+            user = db.execute("SELECT id, name, password FROM users WHERE  password= :inpass",
                     {"inpass": usrpass}).fetchone()
             if user is None:
                 error='The user is not registered'
@@ -31,6 +33,7 @@ def index():
             else:
                 msg='the user exist in the DataBase.'
                 # add user=user
+                session["user_id"]=user.id
                 return render_template('seek.html')            
         if button_click == 'Register':
             user = db.execute("SELECT name, password FROM users WHERE  password= :inpass",
@@ -49,6 +52,12 @@ def index():
 
 @app.route('/seek', methods=['GET', 'POST'])
 def seek():
+    if request.method == 'POST':
+        button_click= request.values.get('logout')
+        if button_click == 'logout': 
+            session.pop("user_id", None)
+            session.pop("book_id",None)
+            return render_template('index.html') 
     seek = request.values.get("seek")
     if seek == "":
         msg= 'You must enter a search model.'
@@ -72,22 +81,60 @@ def seek():
 @app.route('/book', methods=['GET', 'POST'])
 def book():
     if request.method == 'POST':
+        button_click= request.values.get('logout')
+        if button_click == 'logout': 
+            session.pop("user_id", None)
+            session.pop("book_id",None)
+            return render_template('index.html') 
         button_click= request.values.get('button')
-        if button_click == 'review': 
+        if button_click == 'review':
+            print('hace el review') 
             return render_template('review.html') 
         else:
             msg = 'Button rating no action.'
-            return render_template('error.html', message=msg) 
+            return render_template('error.html', message=msg)              
     book_id = request.values.get("book_id")
+    session["book_id"]=book_id
     book = db.execute("SELECT * from goodbooks WHERE id=:book_id",
-                    {"book_id": book_id}).fetchone()
+            {"book_id": book_id}).fetchone()
     res = requests.get (" https://www.goodreads.com/book/review_counts.json", params =
-    {" key ":"dZGnlkLsiELtLnSrHQThzA", "isbns":book.isbn})
+        {" key ":"dZGnlkLsiELtLnSrHQThzA", "isbns":book.isbn})
     data = res.json()
     if book is None:
         msg = 'Book not exist in DataBase'
         return render_template('error.html', message=msg) 
     else:                     
         return render_template('book.html', book=book, data=data)
-    #coments of books there     
+    #coments of books there
+    # 			    
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    if request.method == 'POST':
+        button_click= request.values.get('logout')
+        if button_click == 'logout':
+            session.pop("user_id", None)
+            session.pop("book_id",None)
+            return render_template('index.html') 
+        button_click= request.values.get('button')
+        if button_click == 'Send':
+            opinion = request.values.get('opinion')
+            if opinion is "":
+                msg = 'No make a opinion.'
+                return render_template('error.html', message=msg)
+            else:      
+                print(opinion)
+                book_id=session["book_id"]
+                user_id=session["user_id"]
+                print(book_id)
+                print(user_id)
+                db.execute("INSERT INTO coments (coment, book_id, user_id) VALUES (:coment, :book_id, :user_id)",
+                    {"coment": opinion, "book_id": book_id, "user_id": user_id})
+                db.commit()
+                msg = 'Realiza el send.'
+                return render_template('exito.html', message=msg) 
+        return render_template('review.html')            
+    else:
+        msg = 'No hace POST review.'
+        return render_template('error.html', message=msg)            
+
 app.run()
